69f6f106af21356a2aacb4e1b08c7ccd7c2ae34c
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-24 22:56:35 +0600

    fix: correct typo in appwriteBucketId and ensure proper environment variable definitions

--- appwrite-react/src/conf/config.ts
+++ appwrite-react/src/conf/config.ts
@@ -3,6 +3,6 @@ const conf={
     appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BACKET_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 }
 export default conf
--- appwrite-react/vite-env.d.ts
+++ appwrite-react/vite-env.d.ts
@@ -1,9 +1,15 @@
 /// <reference types="vite/client" />
 
+//ImportMetaEnv: Defines the shape of import.meta.env, listing all available environment variables.
 interface ImportMetaEnv {
-  VITE_API_URL: string; // Add your env variables here
+  readonly VITE_APPWRITE_URL: string;
+  readonly VITE_APPWRITE_PROJECT_ID: string;
+  readonly VITE_APPWRITE_DATABASE_ID: string;
+  readonly VITE_APPWRITE_COLLECTION_ID: string;
+  readonly VITE_APPWRITE_BUCKET_ID: string;
 }
 
+// ImportMeta: Extends the default ImportMeta interface to include the env property, which holds ImportMetaEnv.
 interface ImportMeta {
   env: ImportMetaEnv;
 }
