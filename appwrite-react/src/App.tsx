13bf8cdb7d5489f68d3c56769207b466f5399a4c
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-25 23:21:46 +0600

    refactor: improve type definitions and formatting in App component

--- appwrite-react/src/App.tsx
+++ appwrite-react/src/App.tsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
-import { account, ID } from './appwrite/auth'
+import React, { useState } from "react";
+import { account, ID } from "./appwrite/auth";
+import './App.css'
 
+interface IUser {
+  name: string;
+}
 const App = () => {
-  const [loggedInUser, setLoggedInUser] = useState(null);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [name, setName] = useState('');
+  const [loggedInUser, setLoggedInUser] = useState<IUser | null>(null);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [name, setName] = useState<string>("");
 
-  async function login(email, password) {
+  async function login(email: string, password: string) {
     await account.createEmailPasswordSession(email, password);
     setLoggedInUser(await account.get());
   }
@@ -15,13 +19,28 @@ const App = () => {
   return (
     <div>
       <p>
-        {loggedInUser ? `Logged in as ${loggedInUser.name}` : 'Not logged in'}
+        {loggedInUser ? `Logged in as ${loggedInUser.name}` : "Not logged in"}
       </p>
 
       <form>
-        <input type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
-        <input type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-        <input type="text" placeholder="Name" value={name} onChange={e => setName(e.target.value)} />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
 
         <button type="button" onClick={() => login(email, password)}>
           Login
@@ -40,7 +59,7 @@ const App = () => {
         <button
           type="button"
           onClick={async () => {
-            await account.deleteSession('current');
+            await account.deleteSession("current");
             setLoggedInUser(null);
           }}
         >
