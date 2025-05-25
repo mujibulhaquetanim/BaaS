e0a8dcf8761dbafdb179d53e7164b3ce3ff0cddf
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-25 23:12:26 +0600

    feat: implement authentication module with AppWrite client setup

---
+++ appwrite-react/src/appwrite/auth.ts
@@ -0,0 +1,11 @@
+import { Client, Account} from 'appwrite';
+import conf from '../conf/config';
+
+export const client = new Client();
+
+client
+    .setEndpoint(conf.appwriteUrl)
+    .setProject(conf.appwriteProjectId); 
+
+export const account = new Account(client);
+export { ID } from 'appwrite';
