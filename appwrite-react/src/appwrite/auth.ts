b5c3be417229da277eff80c63b6100e762ea3918
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-26 21:51:31 +0600

    refactor: restructure authentication logic into AuthService class

--- appwrite-react/src/appwrite/auth.ts
+++ appwrite-react/src/appwrite/auth.ts
@@ -1,11 +1,45 @@
-import { Client, Account} from 'appwrite';
+// import { Client, Account} from 'appwrite';
+// import conf from '../conf/config';
+
+// export const client = new Client();
+
+// client
+//     .setEndpoint(conf.appwriteUrl)
+//     .setProject(conf.appwriteProjectId); 
+
+// export const account = new Account(client);
+// export { ID } from 'appwrite';
+
+import { Client, Account, ID } from 'appwrite';
 import conf from '../conf/config';
 
-export const client = new Client();
+export class AuthService {
+    client: Client;
+    account: Account;
+    constructor() {
+        this.client = new Client();
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
 
-client
-    .setEndpoint(conf.appwriteUrl)
-    .setProject(conf.appwriteProjectId); 
+    async signup(email: string, password: string, name: string) {
+        await this.account.create(ID.unique(), email, password, name);
+    }
 
-export const account = new Account(client);
-export { ID } from 'appwrite';
+    async login(email: string, password: string) {
+        await this.account.createEmailPasswordSession(email, password);
+    }
+
+    async logout() {
+        await this.account.deleteSessions();
+    }
+
+    async get() {
+        return this.account.get();
+    }
+}
+
+const authService = new AuthService();
+export default authService;
