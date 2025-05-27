7d42560403a99b578de6c4cd40ea314ddff360c8
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-27 23:04:14 +0600

    refactor: enhance Service class with createPost, updatePost, and deletePost methods for database interactions

--- appwrite-react/src/appwrite/config.ts
+++ appwrite-react/src/appwrite/config.ts
@@ -13,6 +13,46 @@ export class Service{
         this.databases = new Databases(this.client);
         this.buckets = new Storage(this.client);
     }
+
+    // interaction with specific database
+    async createPost({title, slug, content, featuredImage, status}:{title: string, slug: string, content: string, featuredImage: string, status: string}) {
+        try {
+            return await this.databases.createDocument(conf.appwriteDatabaseId, conf.appwriteCollectionId, slug, {
+                title,
+                slug,
+                content,
+                featuredImage,
+                status,
+            });
+        } catch (error) {
+            console.log(`Appwrite createPost error: ${error}`);
+        }
+    }
+
+    async updatePost({title, slug, content, featuredImage, status, userId}:{title: string, slug: string, content: string, featuredImage: string, status: string, userId: string}) {
+        try {
+           return await this.databases.updateDocument(conf.appwriteDatabaseId, conf.appwriteCollectionId, slug, {
+                title,
+                slug,
+                content,
+                featuredImage,
+                status,
+                userId
+            });
+        } catch (error) {
+            console.log(`Appwrite updatePost error: ${error}`);
+        }
+    }
+
+    async deletePost(slug: string) {
+        try {
+            await this.databases.deleteDocument(conf.appwriteDatabaseId, conf.appwriteCollectionId, slug);
+            return true;
+        } catch (error) {
+            console.log(`Appwrite deletePost error: ${error}`);
+            return false;
+        }
+    }
 }
 
 const service = new Service();
