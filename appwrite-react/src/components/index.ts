6ebaca520fcc2e8e4da3e17e6891828e601ff8a2
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-30 22:00:53 +0600

    feat: add Logo component and update index export

---
+++ appwrite-react/src/components/index.ts
@@ -0,0 +1,7 @@
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import Logout from "./header/Logout";
+import Logo from "./Logo";
+import Container from "./container/Container";
+
+export { Header, Footer, Logout, Logo, Container };
